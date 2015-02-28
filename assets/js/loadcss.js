6cfbcd7757e2214c8769343e3ad73b819863271a
Alex Grintsvayg <lyoshenka@gmail.com>
2015-02-28 13:55:26 -0500

    push more css to load asynchronously

--- assets/js/loadcss.js
+++ assets/js/loadcss.js
@@ -2,4 +2,4 @@
 
 function loadCSS(e,t,n,o){"use strict";function r(){for(var e,t=0;t<l.length;t++)l[t].href&&l[t].href.indexOf(i.href)>-1&&(e=!0);e?i.media=n||"all":setTimeout(r)}var i=window.document.createElement("link"),d=t||window.document.getElementsByTagName("script")[0],l=window.document.styleSheets;return i.rel="stylesheet",i.href=e,i.media="only x",o&&(i.onload=o),d.parentNode.insertBefore(i,d),r(),i}
 
-loadCSS("//fonts.googleapis.com/css?family=Open+Sans:400,600,700,400italic,700italic&subset=latin,cyrillic");
+(window.async_css_urls||[]).forEach(function(url) { loadCSS(url);  });
