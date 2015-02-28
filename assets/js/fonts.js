c8b27c9b67eb9ea42158977a3b4ceaf6fc2fda44
Alex Grintsvayg <lyoshenka@gmail.com>
2015-02-28 13:36:45 -0500

    added async js and css loading

---
+++ assets/js/fonts.js
@@ -0,0 +1,5 @@
+// https://github.com/filamentgroup/loadCSS
+
+function loadCSS(e,t,n,o){"use strict";function r(){for(var e,t=0;t<l.length;t++)l[t].href&&l[t].href.indexOf(i.href)>-1&&(e=!0);e?i.media=n||"all":setTimeout(r)}var i=window.document.createElement("link"),d=t||window.document.getElementsByTagName("script")[0],l=window.document.styleSheets;return i.rel="stylesheet",i.href=e,i.media="only x",o&&(i.onload=o),d.parentNode.insertBefore(i,d),r(),i}
+
+loadCSS("//fonts.googleapis.com/css?family=Open+Sans:400,600,700,400italic,700italic&subset=latin,cyrillic");
